8e32c58f63c0edc170ee4a62e78d03bba8c1d520
ender179 <kirilukvlad91@gmail.com>
2024-12-06 12:12:00 +0200

    git push

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import styles from './FriendListItem.module.css';
+import React from 'react';  
+import styles from './FriendListItem.module.css';  
 
-const FriendListItem = ({ avatar, name, isOnline }) => {
-  return (
-    <li className={styles.item}>
-      <span className={isOnline ? styles.online : styles.offline}></span>
-      <img src={avatar} alt="User avatar" className={styles.avatar} />
-      <p className={styles.name}>{name}</p>
-    </li>
-  );
-};
+const FriendListItem = ({ avatar, name, isOnline }) => {  
+  return (  
+    <li className={styles.item}>  
+      <span className={isOnline ? styles.online : styles.offline}></span>  
+      <img src={avatar} alt="User avatar" className={styles.avatar} />  
+      <p className={styles.name}>{name}</p>  
+      <p className={isOnline ? styles.onlineText : styles.offlineText}>  
+        {isOnline ? 'Online' : 'Offline'}  
+      </p>  
+    </li>  
+  );  
+};  
 
 export default FriendListItem;
