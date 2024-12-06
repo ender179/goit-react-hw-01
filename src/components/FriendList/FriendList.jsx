fa6f15f59c34eb1a3f868326f4edce563ca00cf2
ender179 <kirilukvlad91@gmail.com>
2024-12-06 12:23:48 +0200

    git push

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import FriendListItem from '../FriendListItem/FriendListItem';
-import styles from './FriendList.module.css';
+import React from 'react';  
+import FriendListItem from './FriendListItem';  
+import styles from './FriendList.module.css';  
+import friendsData from '../../data/friends.json'; // Підключаємо JSON з даними друзів  
 
-const FriendList = ({ friends }) => {
-  return (
-    <ul className={styles.friendList}>
-      {friends.map(friend => (
-        <FriendListItem
-          key={friend.id}
-          avatar={friend.avatar}
-          name={friend.name}
-          isOnline={friend.isOnline}
-        />
-      ))}
-    </ul>
-  );
-};
+const FriendList = () => {  
+  return (  
+    <ul className={styles.friendList}>  
+      {friendsData.map(friend => (  
+        <FriendListItem  
+          key={friend.id}  
+          avatar={friend.avatar}  
+          name={friend.name}  
+          isOnline={friend.isOnline}  
+        />  
+      ))}  
+    </ul>  
+  );  
+};  
 
 export default FriendList;
