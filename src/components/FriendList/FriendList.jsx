50e41c175e52f3d324a9c79ea4c9b8b9fff1410c
ender179 <kirilukvlad91@gmail.com>
2024-12-06 23:33:31 +0200

    git push

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';  
-import FriendListItem from './FriendListItem';  
+import FriendListItem from './FriendListItem.jsx';  
 import styles from './FriendList.module.css';  
 import friendsData from '../../data/friends.json';
 
