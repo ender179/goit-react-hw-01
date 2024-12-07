669232e09dcb4709a07114ffcba8d02ac7515c62
ender179 <kirilukvlad91@gmail.com>
2024-12-07 11:45:21 +0200

    git push

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import FriendList from './components/FriendList/FriendList';
-import './styles.css';
+import React from 'react';  
+import FriendList from './components/FriendList/FriendList';  
+import Profile from './components/Profile/Profile'; 
+import TransactionHistory from './components/TransactionHistory/TransactionHistory'; 
+import './styles.css'; 
 
-const App = () => {
-  return (
-    <div>
-      <FriendList />
-    </div>
-  );
-};
+const App = () => {  
+  return (  
+    <div>  
+      <h1>Друзья</h1>  
+      <FriendList />  
+      <h1>Профили</h1>  
+      <Profile />  
+      <h1>История транзакций</h1>  
+      <TransactionHistory />  
+    </div>  
+  );  
+};  
 
 export default App;
